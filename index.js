4fb8b3386249ecf2c5f43b823713cbdb9175d2c4
kyachingprue <kyachingpruemarma.info@gmail.com>
2024-12-02 10:47:17 +0600

    new commit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -35,6 +35,13 @@ async function run() {
       res.send(result);
     });
 
+    app.get('/coffee/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await coffeeCollection.findOne(query);
+      res.send(result);
+    });
+
     // POST route to add new coffee
     app.post('/coffee', async (req, res) => {
       const newCoffee = req.body;
@@ -47,6 +54,13 @@ async function run() {
     app.get('/coffee', async (req, res) => {
       const coffees = await coffeeCollection.find({}).toArray();
       res.send(coffees);
+    });
+
+    app.delete('/coffee/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await coffeeCollection.deleteOne(query);
+      res.send(result);
     });
 
     // Ensure connection is alive
